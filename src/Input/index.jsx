65572b4766f71239b5ecd18f1f061bc1cf2864e9
VanessaLin9 <vanessa7591@gmail.com>
2022-09-16 06:13:34 -0400

    form control

--- src/Input/index.jsx
+++ src/Input/index.jsx
@@ -1,45 +1,73 @@
-const eatSomething = ['早餐', '午餐', '晚餐']
+import { useState } from 'react'
+const eatSomething = [{label:'早餐', finish:false}, {label:'午餐', finish:false}, {label:'晚餐', finish:false}]
 
 const Input = ()=> {
+  const [text, setText] = useState('')
+  const [radio, setRadio]= useState('yes')
+  const [checkBox, setCheckBox] = useState(eatSomething)
+  const [select, setSelect] = useState('')
+  const [nameInput, setNameInput] = useState('')
+  const [emailInput, setEmailInput] = useState('')
+  const [base64, setBase64] = useState('')
+
+  const atInputChange= (e) => {
+    const idx = e.target.dataset.index /1
+    const newCheckBoxArr = checkBox.concat()
+    newCheckBoxArr[idx].finish = !newCheckBoxArr[idx].finish
+    setCheckBox(newCheckBoxArr)
+  }
+  const atFileChange = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onload = (evt) => {
+      setBase64(evt.target.result)
+    }
+    reader.readAsDataURL(file)
+  }
+
   return (
   <div className="container">
 
-    
    <form className="inputForm">
-  
     <div className="inputBox">
       <p className="hint">請輸入文字:</p>
        <label htmlFor="">
-        <input type="text" value="" onChange=""/>
+        <input type="text" value={text} onChange={(e)=> setText(e.target.value)}/>
       </label>
     </div>
    
     <div className="inputBox">
-      <p className="hint">Radio選擇:</p>
+      <p className="hint">radio:</p>
       <label htmlFor="">
         <input 
           type="radio" 
           name="radioBoolean"
-          checked=""
-          onChange=""/>yes
+          value="yes"
+          checked={radio ==='yes'}
+          onChange={(e)=> setRadio(e.target.value)}/>yes
       </label>
       <label htmlFor="">
         <input 
         type="radio" 
         name="radioBoolean"
-        value=""
-        checked=""
-        onChange=""/>no
+        value="no"
+        checked={radio ==='no'}
+        onChange={(e)=> setRadio(e.target.value)}/>no
       </label>
     </div>
 
     <div className="inputBox">
       <p className="hint">checkbox:</p>
-       {eatSomething.map((text)=> {
+       {checkBox.map(({label, finish}, index)=> {
         return (
-          <label key={text}>
-            <input type="checkbox" value={text} checked=""  onchange=""/>
-            {text}
+          <label key={label}>
+            <input 
+            data-index={index}
+            type="checkbox" 
+            value={label} 
+            checked={finish}  
+            onChange={atInputChange}/>
+            {label}
           </label>
         )
        })}
@@ -47,7 +75,7 @@ const Input = ()=> {
 
     <div className="inputBox">
       <p className="hint">下拉選單:</p>
-      <select value="" onChange="">
+      <select value={select} onChange={(e)=> setSelect(e.target.value)}>
         <option value="" disabled>請選擇</option>
         <option value="React">React</option>
         <option value="Vue2">Vue2</option>
@@ -55,10 +83,11 @@ const Input = ()=> {
       </select>
     </div>
 
+      {/* TODO validate email */}
     <div className="inputBox">
       <p className="hint">MutiInput:</p>
-        <input type="text" value="" onChange="" placeholder="Name"/>
-        <input type="text" value="" onChange="" placeholder="Email" />
+        <input type="text" value={nameInput} onChange={(e) => setNameInput(e.target.value)} placeholder="Name"/>
+        <input type="text" value={emailInput} onChange={(e)=> setEmailInput(e.target.value)} placeholder="Email" />
     </div>
 
     
@@ -69,8 +98,12 @@ const Input = ()=> {
       <input 
       type="file" 
       accept=".jpg, .png"
-      onChange=""
+      onChange={atFileChange}
       name="file"/>
+      <div className="showImg">
+       {base64 && <img src={base64} alt="" />}
+      </div>
+      
     </div>
    </div>
   )
